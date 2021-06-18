b740b096035eacee264330f183ee303c235c2961
Denno31 <denniskyn80@gmail.com>
2021-06-18 17:37:40 +0300

    combination of 3 numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 48 ; a < 56 ; a++)
+	{
+		for (b = a + 1 ; b < 57 ; b++)
+		{
+			for (c = b + 1 ; c < 58 ; c++)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+				if (a != 55 || b != 56 || c != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
