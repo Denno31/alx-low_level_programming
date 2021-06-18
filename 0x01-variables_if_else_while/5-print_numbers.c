474d5e70e66e0d5123ac5248b38ab137f76e332a
Denno31 <denniskyn80@gmail.com>
2021-06-18 16:14:40 +0300

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int counter;
+
+	while (counter < 10)
+	{
+		printf("%d", counter);
+		counter++;
+	}
+	return (0);
+}
