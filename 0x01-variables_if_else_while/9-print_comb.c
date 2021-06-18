0cbe5aa700807222c2bdc01012d69c1997a15fd0
Denno31 <denniskyn80@gmail.com>
2021-06-18 16:48:57 +0300

    print possible combinations of a number

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = '0';
+	while (c <= '9')
+	{
+		if (c < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
