32eddeac8a062bce1657fd19e637bebfeb09ea64
Denno31 <denniskyn80@gmail.com>
2021-06-24 18:51:53 +0300

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints 0123456789\n
+ *
+ * Return: void
+ */
+
+
+void print_numbers(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; c++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
