d182e8baef3e68a72cf5d0bc44622f24e1237b6b
Denno31 <denniskyn80@gmail.com>
2021-06-28 20:33:42 +0300

    initial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - check the code for ALX School students.
+ * @n:  pointer to be passed in.
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
