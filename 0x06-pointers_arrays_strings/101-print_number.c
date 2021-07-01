584c2bf0d15aa44efc8966bd0cad27d011c10a65
Denno31 <38795170+Denno31@users.noreply.github.com>
2021-07-01 16:23:01 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,29 +1,36 @@
+
 #include "holberton.h"
 
 /**
- * print_number - prints an integer.
- * @n: integer
- * Return: void
+ * print_number - printing number with puts
+ *
+ * @n: input int
  */
-
 void print_number(int n)
 {
-int divisor = 1, i, resp;
+	/*Declaring variables*/
+	int count = 0, pow = 1;
+	unsigned int num = n;
 
-if (n < 0)
-{
-	_putchar('-');
-	n *= -1;
-}
 
-for (i = 0; n / divisor > 9; i++, divisor *= 10)
-;
-
-for (; divisor >= 1; n %= divisor, divisor /= 10)
-{
-	resp = n / divisor;
-	_putchar('0' + resp);
-
-}
-
+	if (n < 0) /*Evaluate this condition*/
+	{
+		_putchar('-');
+		num = -n;
+	}
+	while (n != 0)
+	{
+		n /= 10;
+		count++;
+	}
+	while (count > 1)
+	{
+		pow *= 10;
+		count--;
+	}
+	while (pow >= 1)
+	{
+		_putchar(num / pow % 10 + '0');
+		pow /= 10;
+	}
 }
